957f91d25bc72fa76ac1ee6220438e8c3a13a1a6
Alina <sarpe.alinaa@gmail.com>
2019-11-26 20:14:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// ne legam de id-ul butonului 
+$('#card_btn').click(function(){ add_card(); return false; });
+
+
+function add_card() {
+
+	alert('hello');
+
+	// var container = document.getElementById ("creation_space");
+	// container.innerHTML = nnn
+
+	// .then(function (post) {
+	// 	var img = document.getElementById('top_img').value=post.body;
+	// 	var product_name = document.getElementById('product_name').value= post.title;
+	// 	var price_btn = document.getElementById('price_btn').value= post.user;
+	// 	var add_to_cart = document.getElementById('add_to_cart').value= post.user;
+	// 	document.getElementById("top_img-post").value;
+	// 	document.getElementById("body-product_name").value;
+	// 	document.getElementById("price_btn").value;
+	// 	document.getElementById("body-add_to_cart").value;
+
+	// 	return post.id;
+	//})
+
+}
+
+
