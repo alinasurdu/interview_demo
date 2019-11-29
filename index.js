8cc3e4b6d843e750783326334432916271fbe56b
Alina <sarpe.alinaa@gmail.com>
2019-11-29 20:39:38 +0200

    product list final look

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // ne legam de id-ul butonului 
-$('#card_btn').click(function(){ add_card(); return false; });
+//$('#cart_btn').click(function(){ add_to_cart(); return false; });
 
 
 
 function load_products()
 {
 	var json = [
-		{ "id" : "1", "titlu"   : "til1", "image" : "", "price": "50$"},
-		{ "id" : "1", "titlu"   : "til12", "image" : "", "price": "100$" },
-		{ "id" : "1", "titlu"   : "til3", "image" : "", "price": "70$" },
-		{ "id" : "1", "titlu"   : "til4", "image" : "", "price": "120$" },
-		{ "id" : "1", "titlu"   : "til15", "image" : "", "price": "200$" },
-		{ "id" : "1", "titlu"   : "til1658","image" : "", "price": "180$" }
+		{ "id" : "1", "titlul"   : "Rebecca Mincoff", "image" : "res_8a6203e6765792063c9af2b97f040e6a.jpg", "price": "50$"},
+		{ "id" : "2", "titlul"   : "Rosefield", "image" : "res_44bd29070d68c3d83721edb258d8cfac.jpg", "price": "70$" },
+		{ "id" : "3", "titlul"   : "Tommy Hilfiger", "image" : "res_60a4225bafc7334dc49ee1286c2aa915.jpg", "price": "150$" },
+		{ "id" : "4", "titlul"   : "Oxette", "image" : "res_082f87d34ac77737f031fc7595a76349.jpg", "price": "60$" },
+		{ "id" : "5", "titlul"   : "Tommy Hilfiger", "image" : "res_b6b409bc10972ffc2034c1e69b96d11a.jpg", "price": "180$" },
+		{ "id" : "6", "titlul"   : "Emporio Armani","image" : "res_7ec6a7c170488537e7fa86e9636a7265.jpg", "price": "350$" },
+		{ "id" : "7", "titlul"   : "Fossil","image" : "res_c0030f266f922cca341f744570e63dea.jpg", "price": "110$" },
+		{ "id" : "8", "titlul"   : "Calvin Klein","image" : "res_91360d46990ecc016f9618acfdfa79e1.jpg", "price": "190$" },
+
+
 	];
 	
 	
 	for(var i = 0; i < json.length; i++) 
 	{
 		var obj = json[i];
-		add_card( obj.image,obj.titlu, obj.price);
+		add_card( obj.id, obj.titlul ,obj.image, obj.price);
 	}
 
 }
@@ -27,7 +31,7 @@ function load_products()
 window.addEventListener('load', function(){ load_products();  });
 
 
-function add_card(prod_name, prod_image, prod_price) {
+function add_card(prod_id, prod_name, prod_image, prod_price) {
 
 	//alert('hello');
 	var colomn_body = document.getElementById ("creation_space");
@@ -36,22 +40,20 @@ function add_card(prod_name, prod_image, prod_price) {
 	var new_card = "<div class='card column '>" ;
 
 	new_card += "<div class='row ' id='top_img'><div class='col-md-12'>";
-	new_card +="<img src='images/res_60a4225bafc7334dc49ee1286c2aa915.jpg' class='card-img-top'alt='tommy hilfiger' style='width:100%'>" + prod_image;
+	new_card +="<img src='images/" + prod_image + "'  class='card-img-top'alt='tommy hilfiger' style='width:100%'>";
 	new_card +="</div>";
 	new_card +="</div>";
 	new_card +="<div class='row' id='product_name'>";
-	new_card +="<div class='col-md-12'>";
-	new_card +="<div class='container'>";
-	new_card +="<span class='card-text'>" + prod_name + " </span>";
+	new_card +="<div class='col-md-12 no_padding item_name_align'>";
+	new_card +="<span class='card-text card_font item_name'>" + prod_name + " </span>";
 	new_card +="</div>";
 	new_card +="</div>";
+	new_card +="<div class='row ' id='price_btn'>";
+	new_card +="<div class='col-md-6 no_padding'>";
+	new_card +="<p class='price_style'>" + prod_price + "</p>";
 	new_card +="</div>";
-	new_card +="<div class='row' id='price_btn'>";
-	new_card +="<div class='col-md-6'>";
-	new_card +="<button type='button' class='btn btn-info  btn-sm  '>" + prod_price + "</button>";
-	new_card +="</div>";
-	new_card +="<div class='col-md-6' id='add_to_cart'>";
-	new_card +="<button type='button' class='btn btn-info  btn-sm  fa fa-shopping-cart shopp_btn'></button>";
+	new_card +="<div class='col-md-6 no_padding' id='add_to_cart'>";
+	new_card +="<button type='button' class='btn btn-info  btn-sm  fa fa-shopping-cart shopp_btn' onClick='add_to_cart(\""+prod_id+ "\",  \"" +prod_name +"\" , \"" + prod_image +"\" , \""+prod_price+"\" );' ></button>";
 	new_card +="</div>";
 	new_card +="</div>";
 	new_card +="</div>";
@@ -62,21 +64,29 @@ function add_card(prod_name, prod_image, prod_price) {
 
 	colomn_body.innerHTML = result;
 
-	// var container = document.getElementById ("creation_space");
-	// container.innerHTML = nnn
+	
+}
 
-	// .then(function (post) {
-	// 	var img = document.getElementById('top_img').value=post.body;
-	// 	var product_name = document.getElementById('product_name').value= post.title;
-	// 	var price_btn = document.getElementById('price_btn').value= post.user;
-	// 	var add_to_cart = document.getElementById('add_to_cart').value= post.user;
-	// 	document.getElementById("top_img-post").value;
-	// 	document.getElementById("body-product_name").value;
-	// 	document.getElementById("price_btn").value;
-	// 	document.getElementById("body-add_to_cart").value;
 
-	// 	return post.id;
-	//})
+
+function add_to_cart(id, titlul, image_name, price) {
+
+	//alert('hello');
+	var colomn_body = document.getElementById ("cart_space");
+	//console.log(colomn_body.innerHTML); 
+
+	var new_card = " <li>" + titlul + "</li>"  ;
+	new_card += " <li class='li_aling'>" + price +"</li>"  ;
+	new_card += " <li> ---- </li>"  ;
+
+	
+	var result = colomn_body.innerHTML + new_card; 
+	//console.log(result); 
+
+
+	colomn_body.innerHTML = result;
+
+	
 
 }
 
